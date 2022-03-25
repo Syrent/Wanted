fb8cc97b83b23cfecf75ab0eb970277a1de005a1
Syrent <Syrent2356@gmail.com>
2022-03-25 10:41:50 +0430

    2.3.8 | Prevent throwing NullPointerException when the region is null

--- src/main/java/ir/syrent/wanted/GUI/RequestGUI.java
+++ src/main/java/ir/syrent/wanted/GUI/RequestGUI.java
@@ -75,7 +75,7 @@ public class RequestGUI implements Listener {
                                     player.getLocation().getY(),
                                     player.getLocation().getZ())))
                     .replace("%last_victim%", victim == null ? "Unknown" : victim)
-                    .replace("%region%", Main.worldGuard == null ? "UNKNOWN" : Main.worldGuard.getRegionName(player.getLocation())));
+                    .replace("%region%", Main.worldGuard == null ? "UNKNOWN" : Main.worldGuard.getRegionName(player.getLocation()) == null ? "UNKNOWN" : Main.worldGuard.getRegionName(player.getLocation())));
             }
             meta.setLore(lore);
 
